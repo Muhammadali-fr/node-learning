6bcf1f01f0c8fc7197d7ff486d3fc7f4e6a93fef
Jamolov Muhammadali <jamolovmuhammadali18@gmail.com>
2024-12-29 17:38:50 +0500

    route parametrlar

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get("/api/books", (req, res) => {
   res.send(["abadiyot", "oq soyloq", "oqish"]);
 });
 
+app.get('/api/books/:id' , (req, res) => {
+  res.send(req.params.id)
+})
+
+app.get('/api/articles/:year/:month' , (req, res) => {
+  res.send(req.query)
+})
+
 const port = process.env.PORT || 5173
 
 app.listen(port, () => {
