224ee12bc6de12e3ba9f45c19efe72feb95b54a6
Jamolov Muhammadali <jamolovmuhammadali18@gmail.com>
2024-12-29 11:23:53 +0500

    learning express js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-let _ = require("underscore");
-let res = _.contains([10, 13], 13);
-console.log(res);
+let express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Salom");
+});
+
+app.get("/api/books", (req, res) => {
+  res.send(["abadiyot", "oq soyloq", "oqish"]);
+});
+
+const port = process.env.PORT || 5173
+
+app.listen(port, () => {
+  console.log(` ${port} - chi portni eshitishni boshladm...`);
+});
