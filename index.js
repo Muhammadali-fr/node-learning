b9f9a9cad2c23566d21e6b1e1c0cae359a9b6019
Jamolov Muhammadali <jamolovmuhammadali18@gmail.com>
2024-12-30 11:18:17 +0500

    amaliy

--- index.js
+++ index.js
@@ -4,19 +4,30 @@ app.get("/", (req, res) => {
   res.send("Salom");
 });
 
+const books = [
+  { id: 1, name: "edewedwedwedewdwe" },
+  { id: 2, name: "oq soyloq" },
+  { id: 3, name: "oqish" },
+];
+
 app.get("/api/books", (req, res) => {
   res.send(["abadiyot", "oq soyloq", "oqish"]);
 });
 
-app.get('/api/books/:id' , (req, res) => {
-  res.send(req.params.id)
-})
+app.get("/api/books/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
 
-app.get('/api/articles/:year/:month' , (req, res) => {
-  res.send(req.query)
-})
+  if (!book) {
+    return res.status(404).send("Bu kitob topilmadi!!!");
+  }
+  res.send(book);
+});
 
-const port = process.env.PORT || 5173
+app.get("/api/articles/:year/:month", (req, res) => {
+  res.send(req.query);
+});
+
+const port = process.env.PORT || 5173;
 
 app.listen(port, () => {
   console.log(` ${port} - chi portni eshitishni boshladm...`);
