d21d6a47f4c6c12e158cbf41439bfa9c6cbf2abb
Jamolov Muhammadali <jamolovmuhammadali18@gmail.com>
2024-12-30 22:39:37 +0500

    Crud

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let express = require("express");
-const Joi = require('joi');
+const Joi = require("joi");
 const app = express();
 app.use(express.json());
 
@@ -14,24 +14,19 @@ const books = [
 ];
 
 app.get("/api/books", (req, res) => {
-  res.send(["abadiyot", "oq soyloq", "oqish"]);
+  res.send(books);
 });
 
-app.post("api/books", (req, res) => {
+app.post("/api/books", (req, res) => {
+  const bookSchedule = Joi.object({
+    name: Joi.string().required().min(3),
+  });
 
-  const bookSchedule = Joi.object(
-    {
-      name: Joi.string().required().min(3)
-    }
-  )
+  const { error } = bookSchedule.validate(req.body);
 
-  const {error} = bookSchedule.validate(req.body)
-  
-
-  if(error){
+  if (error) {
     return res.status(400).send(error.details[0].message);
   }
-
 
   const newBook = {
     id: books.length + 1,
@@ -51,8 +46,35 @@ app.get("/api/books/:id", (req, res) => {
   res.send(book);
 });
 
-app.get("/api/articles/:year/:month", (req, res) => {
-  res.send(req.query);
+app.put("/api/books/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (!book) {
+    return res.status(404).send("this id's book is not found!");
+  }
+
+  const bookSchedule = Joi.object({
+    name: Joi.string().required().min(3),
+  });
+
+  const { error } = bookSchedule.validate(req.body);
+  if (error) {
+    return res.status(404).send(error.details[0].message);
+  }
+
+  book.name = req.body.name;
+  res.send(book);
+});
+
+app.delete("/api/books/:id", (req, res) => {
+  const book = books.find((b) => b.id === parseInt(req.params.id));
+  if (!book) {
+    return res.status(404).send("bu id ga mos kitob topilmdi!!!");
+  }
+
+  const bookIndex = books.indexOf(book);
+  books.splice(bookIndex, 1);
+
+  return res.send(book);
 });
 
 const port = process.env.PORT || 5173;
